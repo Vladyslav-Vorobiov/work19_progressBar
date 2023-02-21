29581dc648061679595f048e150702f03eb4551b
VladITGod <vorobiov.v90@gmail.com>
2023-02-21 21:32:09 +0200

    more new styles for progressbar added

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -8,11 +8,7 @@ class ProgressBar extends React.Component {
         }
         return (
              <div className="progress m-3">
-<<<<<<< HEAD
                  <div className="progress-bar progress-bar-striped progress-bar-animated bg-info"
-=======
-                 <div className="progress-bar progress-bar-striped bg-info"
->>>>>>> 8a806475368f579654546c3033a270f6edcbc291
                      role="progressbar"
                      aria-valuenow={percentage}
                      aria-valuemin="0"
