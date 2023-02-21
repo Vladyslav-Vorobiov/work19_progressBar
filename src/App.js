9d1a8506cbb90266377fac5f4023432f26653e23
VladITGod <vorobiov.v90@gmail.com>
2023-02-21 20:03:18 +0200

    component Progress added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Progress from './components/ProgressBar'
 function App() {
   return (
     <div className="App">
-
+        <h2 className="m-3">Progress bar</h2>
+      <Progress percentage={40} />
     </div>
   );
 }
